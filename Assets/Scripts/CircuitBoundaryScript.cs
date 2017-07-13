e64113af6484d081e09a12cbc67c221dab332f4d
andy-j92 <andyjang0530@gmail.com>
2017-07-14 01:12:07 +1200

    change layout and component draggable

---
+++ Assets/Scripts/CircuitBoundaryScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircuitBoundaryScript : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D other)
+    {
+        other.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    //void OnTriggerStay2D(Collider2D other)
+    //{
+    //    other.gameObject.GetComponent<SpriteRenderer>().enabled = true;
+    //}
+
+    void OnTriggerExit2D (Collider2D other)
+    {
+        Debug.Log(other);
+        other.gameObject.GetComponent<SpriteRenderer>().enabled = false;
+    }
+}
