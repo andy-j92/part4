78674de9a3b438ed59248a84f1848ba1bac93c48
andy-j92 <andyjang0530@gmail.com>
2017-07-18 01:48:12 +1200

    commit to fix branching error

---
+++ Assets/Scripts/HistoryManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HistoryManager : MonoBehaviour {
+
+    public int col, row;
+	// Use this for initialization
+	void Start () {
+        var parent = gameObject.GetComponent<RectTransform>();
+        var grid = gameObject.GetComponent<GridLayoutGroup>();
+        grid.cellSize = new Vector2(parent.rect.width / col, parent.rect.height / row);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
