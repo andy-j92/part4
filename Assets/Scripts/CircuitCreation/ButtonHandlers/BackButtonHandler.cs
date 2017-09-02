7feeb1b7521b00e97967291ac41d3b0710b582d6
andy-j92 <andyjang0530@gmail.com>
2017-09-03 01:30:57 +1200

    reset selected resistors when leaving scene

--- Assets/Scripts/CircuitCreation/ButtonHandlers/BackButtonHandler.cs
+++ Assets/Scripts/CircuitCreation/ButtonHandlers/BackButtonHandler.cs
@@ -8,6 +8,8 @@ public class BackButtonHandler : MonoBehaviour {
 	public void MoveToGameMenuScreen()
     {
         SceneManager.LoadScene("GameMenu");
+        CircuitHandler.selected1 = null;
+        CircuitHandler.selected2 = null;
     }
 
     public void MoveToPracticeScreen()
